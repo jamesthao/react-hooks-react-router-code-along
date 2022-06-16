df7377d0652540c9e5e78c279c5561aa79bfdae7
James <jamesthao32@gmail.com>
2022-06-15 20:49:27 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
+ReactDOM.render( <Router> <App/></Router>
+, document.getElementById("root"));
